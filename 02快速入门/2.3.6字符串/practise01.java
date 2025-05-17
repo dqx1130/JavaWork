34474c26f467a56df6fb756ef7c199983768b5ff
Your Name <you@example.com>
2025-05-18 00:50:20 +0800

    20250518

---
+++ 02快速入门/2.3.6字符串/practise01.java
@@ -0,0 +1,9 @@
+public class practise01 {
+    public static void main(String[] args){
+        int a = 72;
+        int b = 105;
+        int c = 65281;
+        String s = "" + (char)a + (char)b + (char)c ;
+        System.out.println(s);
+    }
+}
