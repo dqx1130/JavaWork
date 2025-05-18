e089134ae267a5eb34acf05e2bdf29301c10903f
Your Name <you@example.com>
2025-05-18 13:17:56 +0800

    Lastest

---
+++ 02快速入门/2.4.2if条件判断/example01.java
@@ -0,0 +1,12 @@
+public class example01 {
+    public static void main(String[] args){
+        int n = 90;
+        if (n >= 90) {
+            System.out.println("A");
+        } else if (n >= 60) {
+            System.out.println("B");
+        } else {
+            System.out.println("C");
+        }
+    }
+}
