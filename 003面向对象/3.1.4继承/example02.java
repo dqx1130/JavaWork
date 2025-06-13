cbb23c2b20c3e23e4144d6405a5313fe62c9a860
Your Name <you@example.com>
2025-06-13 12:15:01 +0800

    20250613

--- 003面向对象/3.1.4继承/example02.java
+++ 003面向对象/3.1.4继承/example02.java
@@ -1,2 +1,23 @@
+//必须使用super
+class Person1{
+    protected String name;
+    protected int age;
+
+    public Person1(String name,int age){
+        this.age = age;
+        this.name = name;
+    }
+}
+class Student1 extends Person1{
+    protected int score;
+    public Student1(String name , int age ,int score){
+        super(name,age);
+        this.score = score;
+    }
+}
+
 public class example02 {
+    public static void main(String[] args){
+        Student1 s = new Student1("AK" , 12,140);
+    }
 }
